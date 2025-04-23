2ddbc71b5d20e8e8a23c50043ccfd01665a6837f
AlexanderKlar <alexanderwklar@gmail.com>
2025-04-22 23:22:09 -0400

    Update middleware configuration for Vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,11 @@ import { authMiddleware } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in", "/sign-up"],
+  ignoredRoutes: ["/api/trpc", "/_next", "/favicon.ico"]
+});
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 }; 
