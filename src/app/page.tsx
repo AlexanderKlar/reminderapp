49132c4b2bc12eff06657b28dd2a1441bcdccffa
AlexanderKlar <alexanderwklar@gmail.com>
2025-04-22 21:07:18 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import ReminderApp from '../components/ReminderApp';
-
 export default function Home() {
   return (
-    <main className="min-h-screen p-4 bg-gray-100">
-      <ReminderApp />
+    <main className="min-h-screen p-4">
+      <h1>Hello World</h1>
     </main>
   );
 }
