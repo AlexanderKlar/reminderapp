0a02c28dce1a7d234c60e477d5b6aa4fc172f9dc
AlexanderKlar <alexanderwklar@gmail.com>
2025-04-22 23:26:17 -0400

    Update middleware configuration for Vercel deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 export default authMiddleware({
   publicRoutes: ["/", "/sign-in", "/sign-up"],
-  ignoredRoutes: ["/api/trpc", "/_next", "/favicon.ico"]
+  ignoredRoutes: ["/api/trpc", "/_next", "/favicon.ico", "/api/webhook"]
 });
 
 export const config = {
