ccfb6e166b1a305ef72bd0b76a82d8065b960206
AlexanderKlar <alexanderwklar@gmail.com>
2025-04-22 21:10:37 -0400

    Re-integrate ReminderApp component into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import ReminderApp from '../components/ReminderApp';
+
 export default function Home() {
   return (
-    <main className="min-h-screen p-4">
-      <h1>Hello World</h1>
+    <main className="min-h-screen p-4 bg-gray-100">
+      <ReminderApp />
     </main>
   );
 }
